62d21597305efbd6ae57ce19c2c845a055f9615f
GuiOliveira98 <guil.oliveira98@gmail.com>
2023-07-24 01:08:46 -0300

    created view project

--- src/pages/ViewProject.tsx
+++ src/pages/ViewProject.tsx
@@ -4,9 +4,79 @@ import { faDownload, faEdit } from "@fortawesome/free-solid-svg-icons";
 import { PrimaryButton, SecondaryButton } from "../ds/Buttons";
 import { Container } from "../ds/Container";
 import { Project } from "../schemas";
+import { useState } from "react";
+import { generateChartUrl } from "./CreateProject";
+
+function format(value: number, fractionDigits: number = 0) {
+  return value.toLocaleString("pt-BR", {
+    maximumFractionDigits: fractionDigits,
+  });
+}
+
+function formatMoney(value: number) {
+  return `R$ ${format(value)}`;
+}
+
+function Accordion({
+  items,
+}: {
+  items: { header: string; content: React.ReactNode }[];
+}) {
+  const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
+
+  return (
+    <div className="accordion mb-3">
+      {items.map(({ header, content }, index) => (
+        <div
+          className="accordion-item"
+          key={index}
+          onClick={() => setSelectedItemId((s) => (s === index ? null : index))}
+        >
+          <h2 className="accordion-header">
+            <button
+              className={
+                `accordion-button` +
+                (selectedItemId === index ? "" : " collapsed")
+              }
+              type="button"
+            >
+              {header}
+            </button>
+          </h2>
+
+          <div
+            className={
+              `accordion-collapse collapse` +
+              (selectedItemId === index ? " show" : "")
+            }
+          >
+            <div className="accordion-body">{content}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function Info({ data }: { data: Record<string, string | number> }) {
+  return (
+    <table className="table table-hover">
+      <tbody>
+        {Object.entries(data).map(([label, value], index) => (
+          <tr key={index}>
+            <th scope="row">{label}</th>
+            <td>{value}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 export function ViewProject(state: ViewProjectPage) {
   const { editProject } = useRoot();
+
+  const p = state.project;
   return (
     <>
       <NavBar />
@@ -14,16 +84,83 @@ export function ViewProject(state: ViewProjectPage) {
         <h2>
           {state.project.id} - {state.project.name}
         </h2>
-        <table className="table table-hover">
-          <tbody>
-            <tr>
-              <th scope="row">Identificação</th>
-              <td>
-                {state.project.id} - {state.project.name}
-              </td>
-            </tr>
-          </tbody>
-        </table>{" "}
+        <Accordion
+          items={[
+            {
+              header: "Identificação",
+              content: (
+                <Info
+                  data={{
+                    Código: `${p.id} - ${p.name}`,
+                    Nome: p.name,
+                    Região: p.location,
+                  }}
+                />
+              ),
+            },
+
+            {
+              header: "Projeto",
+              content: (
+                <Info
+                  data={{
+                    "Consumo anual": formatMoney(p.yearlyConsumption),
+                    Concessionária: p.powerDistributionCompany,
+                    Tensão: p.tension,
+                    "Disjuntor (A)": p.circuitBreakerAmp,
+                    Telhado: p.roofType,
+                    "Geração estimada ao ano": `${p.necessaryArea} kWp`,
+                    "Área necessária": `${p.necessaryArea} m²`,
+                    "Potência usina": `${p.potency} kWp`,
+                    "Quantidade de módulos": `${p.nrOfModules} módulos`,
+                  }}
+                />
+              ),
+            },
+
+            {
+              header: "Análise de projeto",
+              content: (
+                <>
+                  <Info
+                    data={{
+                      "Código do UG": p.ugId,
+                      "Possui UC?": p.hasUc ? "SIM" : "NÃO",
+                      Grupo: p.groupType,
+                      Tarifa: formatMoney(p.fare),
+                    }}
+                  />
+                  <img
+                    alt="chart"
+                    src={generateChartUrl(
+                      "zm-0b636365-94ac-4e4f-bed2-ffdaa2f3e2ff",
+                      [p.energyConsumption, p.energyProduction]
+                    )}
+                  />
+                </>
+              ),
+            },
+
+            {
+              header: "Investimento e simulação financeira",
+              content: (
+                <Info
+                  data={{
+                    "Investimento total": formatMoney(p.investment.totalValue),
+                    "Valor específico": formatMoney(p.investment.specifics),
+                    Payback: p.investment.paybackRate,
+                    TIR: p.investment.tirPercentage,
+                    VPL: p.investment.vplValue,
+                    "FC. Período": p.investment.fcPeriod,
+                    "Simulação Banco": p.investment.bankSimulation,
+                    Parcelas: p.investment.nrOfPayments,
+                    "Valor da parcela": p.investment.monthlyPaymentValue,
+                  }}
+                />
+              ),
+            },
+          ]}
+        />
         <SecondaryButton
           text="Editar projeto"
           onClick={() => editProject(state.project)}
